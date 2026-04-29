a941f86de49b44dd53f56e2112b4fb9b750eba14
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-04-29 21:47:10 +0600

    Navbar and Footer completed

---
+++ src/Components/Shared/Footer/Footer.jsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import React from 'react';
+import { FaFacebookSquare, FaInstagramSquare, FaPhoneAlt } from 'react-icons/fa';
+import { FaLocationDot, FaXTwitter } from 'react-icons/fa6';
+import { IoIosMail } from 'react-icons/io';
+
+const Footer = () => {
+    return (
+
+        <div className='bg-linear-to-br from-orange-50 via-pink-50 to-orange-50 border-t border-orange-100 p-10'>
+
+        <div className='grid grid-cols-4 gap-8 container mx-auto'>
+            <div className='space-y-2'>
+                <h2 className="bg-linear-to-r from-orange-500 to-pink-500 text-2xl text-transparent bg-clip-text font-bold">SunCart</h2>
+                <p className='text-gray-600'>Your one-stop shop for all summer essentials. Quality products for unforgettable summer moments.</p>
+            </div>
+            <div className='space-y-2'>
+                <h2 className='text-xl font-bold'>Quick Links</h2>
+                <ul>
+                    <li><Link href={"/"} className='cursor-pointer text-gray-600 hover:text-orange-600'>Home</Link></li>
+                    <li><Link href={"/products"} className='cursor-pointer text-gray-600 hover:text-orange-600'>Products</Link></li>
+                    <li><Link href={"/profile"} className='cursor-pointer text-gray-600 hover:text-orange-600'>My Profile</Link></li>
+                    
+                </ul>
+            </div>
+            <div className='space-y-2'>
+                 <h2 className='text-xl font-bold'>Contact Info</h2>
+                 <ul>
+                    <li className='text-gray-600 flex items-center gap-2'><IoIosMail className='text-orange-600' /><span>support@suncart.com</span></li>
+                    <li className='text-gray-600 flex items-center gap-2'><FaPhoneAlt  className='text-orange-600' /><span>+1 (555) 123-4567</span></li>
+                    <li className='text-gray-600 flex items-center gap-2'><FaLocationDot className='text-orange-600' /><span>Miami Beach, FL</span></li>
+                 </ul>
+            </div>
+            <div className='space-y-3'>
+                <h2 className='text-xl font-bold'>Follow Us</h2>
+                <div className='flex items-center gap-3'>
+                    <FaFacebookSquare />
+                    <FaInstagramSquare />
+                    <FaXTwitter />
+                </div>
+                <p className='text-gray-600'>Privacy Policy</p>
+            </div>
+        </div>
+        <div className='divider divider-warning'></div>
+        <p className='text-gray-600 text-center'>© 2026 SunCart. All rights reserved. Made with ❤️ for summer lovers.</p>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+    
+  return (
+    <div className="  bg-base-100 shadow-sm">
+
+    <div className="container mx-auto navbar">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          <ul
+            tabIndex="-1"
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <Link href={"/"} className="hover:bg-orange-500 hover:text-white font-medium">Home</Link>
+            </li>
+            <li>
+              <Link href={"/products"} className="hover:bg-orange-500 hover:text-white font-medium">Products</Link>
+              
+            </li>
+            <li>
+              <Link href={"/"} className="hover:bg-orange-500 hover:text-white font-medium">My Profile</Link>
+            </li>
+          </ul>
+        </div>
+        <Link href={"/"} className=" bg-linear-to-r from-orange-500 to-pink-500 text-2xl text-transparent bg-clip-text font-bold">SunCart</Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+           <li>
+              <Link href={"/"} className="hover:bg-orange-500 hover:text-white font-medium">Home</Link>
+            </li>
+            <li>
+              <Link href={"/products"} className="hover:bg-orange-500 hover:text-white font-medium">Products</Link>
+              
+            </li>
+            <li>
+              <Link href={"/profile"} className="hover:bg-orange-500 hover:text-white font-medium">My Profile</Link>
+            </li>
+        </ul>
+      </div>
+      <div className="navbar-end space-x-2.5">
+        <button className="px-5 py-2.5 rounded-lg text-orange-600 font-medium hover:bg-orange-50 cursor-pointer transition-colors">Login</button>
+        <button className="px-5 py-2.5 bg-linear-to-r from-orange-500 to-pink-500 text-white rounded-lg hover:shadow-lg cursor-pointer font-medium  transition-all">Register</button>
+      </div>
+    </div>
+    </div>
+  );
+};
+
+export default Navbar;
