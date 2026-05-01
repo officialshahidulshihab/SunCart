4288121ae16a0471cdeeaf091d741293cf2f029b
officialshahidulshihab <officialshahidulshihab@gmail.com>
2026-05-01 20:29:28 +0600

    email related problem sol

--- src/app/(main)/profile/page.jsx
+++ src/app/(main)/profile/page.jsx
@@ -24,7 +24,7 @@ const ProfilePage = () => {
     const { data: res, error } = await authClient.updateUser({
       image: image,
       name: name,
-      email: email,
+      newEmail: email,
     });
     if (error) {
       setModalError(error.message || "Something went wrong.");
